5af9f621e4a4a29d20344a7b68934b9dde4aa61c
Deadpool <abcd162003@gmail.com>
2022-03-27 03:17:11 +0530

    added index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,23 @@ import App from "./App";
 import "./index.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-<<<<<<< HEAD
 import { ChakraProvider } from "@chakra-ui/react";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import { reducers } from "./reducer";
-import "mapbox-gl/dist/mapbox-gl.css";
-import Map from "./components/newproject/Map";
-import Landing from "./components/Landing/Landing";
-=======
->>>>>>> b6da0846b06c0e1a81023f210c2372ab525b8138
 
+import { Provider } from "react-redux";
+
+import { createStore, applyMiddleware, compose } from "redux";
+
+import thunk from "redux-thunk";
+
+import { reducers } from "./reducer";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import Map from "./components/newproject/Map";
+
+import Landing from "./components/Landing/Landing";
+
+import Dashboard from "./components/Worker/WorkerDashboard/dashboard";
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
